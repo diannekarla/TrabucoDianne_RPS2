323461e6babc1caf3d95104d1169645d1b06ddc9
Dianne Karla Trabuco <diannekarlatrabuco@Diannes-Air.phub.net.cable.rogers.com>
2021-10-20 16:31:08 -0400

    adding python game

---
+++ main.py
@@ -0,0 +1,13 @@
+from random import randint 
+
+choices = ["rock", "paper", "scissors"]
+
+# player will be the weapon the player chooses via input
+player = input("Choose your weapon: rock, paper or scissors: " )
+
+computer = choices[randint(0,2)]
+
+print("player chose: " + player)
+print("computer chose: " + computer)
+
+
